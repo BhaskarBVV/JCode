f0ef3e1101741e634b3624c6593926da198396c2
BhaskarBVV <71118230+BhaskarBVV@users.noreply.github.com>
2022-01-21 00:26:18 +0530

    Create 2)First Occurrence of any value in arary by recursion.java

---
+++ Recursion/2)First Occurrence of any value in arary by recursion.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+public class editor3 {
+    public static Scanner sc = new Scanner(System.in);
+
+    public static int firstOccur(int[] a, int val, int i) {
+        if (i + 1 == a.length)
+            return -1;
+        if (a[i] == val)
+            return i;
+        return firstOccur(a, val, i + 1);
+    }
+
+    static void solve() {
+        int n = sc.nextInt();
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++)
+            a[i] = sc.nextInt();
+        System.out.println("Enter the value whose first index is to be calculated: ");
+        int val = sc.nextInt();
+        System.out.println(firstOccur(a, val, 0));
+    }
+
+    public static void main(String[] args) {
+        int n;
+        n = 1;
+        // n = sc.nextInt();
+        while (n-- > 0) {
+            solve();
+        }
+        sc.close();
+    }
+}
