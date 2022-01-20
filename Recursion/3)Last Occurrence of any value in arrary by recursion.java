34c721ab3e9c26b2f0c00102151d0e3a424dd0f0
BhaskarBVV <71118230+BhaskarBVV@users.noreply.github.com>
2022-01-21 00:31:44 +0530

    Create 3)Last Occurrence of any value in arrary by recursion.java

---
+++ Recursion/3)Last Occurrence of any value in arrary by recursion.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+public class editor3 {
+    public static Scanner sc = new Scanner(System.in);
+
+    public static int firstOccur(int[] a, int val, int i) {
+        if (i == -1)
+            return -1;
+        if (a[i] == val)
+            return i;
+        return firstOccur(a, val, i - 1);
+    }
+
+    static void solve() {
+        int n = sc.nextInt();
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++)
+            a[i] = sc.nextInt();
+        System.out.println("Enter the value whose last index is to be calculated: ");
+        int val = sc.nextInt();
+        System.out.println(firstOccur(a, val, n - 1));
+    }
+
+    public static void main(String[] args) {
+        int n;
+        n = 1;
+        // n = sc.nextInt();
+        while (n-- > 0) {
+            solve();
+        }
+        sc.close();
+    }
+}
